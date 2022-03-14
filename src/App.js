6f2afc5e6d5cdaec03517f57d48b78def7ed4e5b
Mar-Issah <masy370@gmail.com>
2022-03-14 17:26:01 -0400

    Handle slider function

--- src/App.js
+++ src/App.js
@@ -1,64 +1,62 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Thumbs from './components/Thumbs';
 import Viewer from './components/Viewer';
 import './index.css';
 import { slideData as images } from './data';
 
 function App() {
-  const delay = 2500;
-  const [index, setIndex] = useState(1);
-  const [slider, setSlider] = useState(1);
-  const checkboxRef = useRef();
+  const delay = 2000;
+  const [index, setIndex] = useState(0);
+  const [isSlider, setSlider] = useState(false);
+  const checkboxRef = useRef(null);
 
   const handleNext = () => {
-    if (index < images.length - 1) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0);
+    if (!isSlider) {
+      if (index < images.length - 1) {
+        setIndex(index + 1);
+      } else {
+        setIndex(0);
+      }
+      console.log('running...');
     }
   };
 
   /*======= Previous function to move slider backward ======= */
   const handlePrev = () => {
-    if (index === 0) {
-      setIndex(images.length - 1);
-    } else {
-      setIndex((index) => index - 1);
+    if (!isSlider) {
+      if (index === 0) {
+        setIndex(images.length - 1);
+      } else {
+        setIndex((index) => index - 1);
+      }
     }
   };
 
-  /*======= Function to move slider base on its index ======= */
-  const current = (curIndex) => {
-    if (index !== curIndex) {
-      setIndex(curIndex);
+  const handleSlider = () => {
+    setSlider(true);
+    setIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handleCheckbox = (e) => {
+    console.log(e.target.checked);
+    if (e.target.checked) {
+      checkboxRef.current = setInterval(handleSlider, delay);
+      console.log('isChecked');
+    } else {
+      console.log('not checked');
+      clearInterval(checkboxRef.current);
+      setSlider(false);
     }
   };
-  useEffect(() => {
-    let timer = setInterval(() => {}, 5000);
-    //intervalRef.current = timer;
-    const handleCheckbox = (e) => {
-      if (checkboxRef.current.checked) {
-        console.log('isChecked');
-      } else {
-        console.log('not checked');
-      }
-    };
-    return () => {
-      clearInterval();
-    };
-  });
 
   return (
     <div>
       <Viewer image={images[index]} />
       <Thumbs images={images} setIndex={setIndex} />
       <label>
-        <input
-          type='checkbox'
-          ref={checkboxRef}
-          id='myCheck'
-          onClick={handleCheckbox}
-        />
+        <input type='checkbox' ref={checkboxRef} onClick={handleCheckbox} />
         Click Checkbox to start slider
       </label>
       <br />
